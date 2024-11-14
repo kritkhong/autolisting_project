f7de5e4b75204a0f3023a08d20e2d4b5524f826b
Kritkhong <kritkhong@gmail.com>
2024-11-14 19:49:24 +0700

    change ocr to PPOCR for more acceptable accuracy

--- auto_listing.py
+++ auto_listing.py
@@ -1,4 +1,4 @@
-import easyocr
+from paddleocr import PaddleOCR, draw_ocr
 from PIL import Image
 import PIL
 from pathlib import Path
@@ -20,6 +20,8 @@ result_folder.mkdir(exist_ok=True)
 # makes image's width/height limited to {size_max} px, remain original ratio
 img_size = 500
 
+ocr = PaddleOCR(lang='en')  # need to run only once to load model into memory
+
 # iterate trough 'image' files in folder
 for item in sorted(dir_path.iterdir()):
     if not item.is_dir():
@@ -37,16 +39,23 @@ for item in sorted(dir_path.iterdir()):
         resized_img.save(output_name)
 
 #       item_info = [ {code} , {caption}, {price}]
-        reader = easyocr.Reader(['en'])
-        results = reader.readtext(output_name)
 
-        row = []
-        for word in results:
-            row.append((word[1], f'{round(word[2], 2)*100}%'))
-        print(row)
+        result = ocr.ocr(output_name, cls=False)
+        result = result[0]
+        print(f'------------------{item.stem}---------------------')
+        for line in result:
+            print(line)
 
-        '''TRY PPOCR library maybe more accurate'''
-
+        ############ THIS TO SAVE NEW FILE WITH OCR DETECTION #############
+        # image = Image.open(output_name).convert('RGB')
+        # boxes = [line[0] for line in result]
+        # txts = [line[1][0] for line in result]
+        # scores = [line[1][1] for line in result]
+        # im_show = draw_ocr(image, boxes, txts, scores,
+        #                    font_path='/Users/krit/Desktop/cs/project/autolisting/font/simfang.ttf')
+        # im_show = Image.fromarray(im_show)
+        # im_show.save(f'{result_folder}/{item.stem}_ocr.jpg')
+        # ##############################################################
 
 #       extract code from image, price ->  item_info <-----------***** how to indicate RegEx?
 #       rename img to code
